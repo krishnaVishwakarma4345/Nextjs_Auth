8d370b997e9165bfe8280b64edb71d6c9f58c929
krishnaVishwakarma4345 <krishnavishwakarma4345@gmail.com>
2026-02-09 15:33:49 +0530

    logout added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,23 @@
+"use client"
+
 import React from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 function page() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      const response =await axios.get("/api/users/logout");
+       router.push("/login");
+    } catch (error) {
+      console.error("Logout failed:", error); 
+    }
+  }
   return (
-    <div>profile</div>
+    <div>profile
+      <button onClick={logout}>Logout</button>
+    </div>
   )
 }
 
